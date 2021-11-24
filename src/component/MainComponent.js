9a04f4a50d33244af7ad9aa92f596e5f9549b4d1
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2021-11-24 14:57:54 +0700

    React Animation

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -10,6 +10,7 @@ import {Route,Switch,Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import { addComment, fetchComments, fetchDishes, fetchPromos,postComment} from '../redux/ActionCreator';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps=state=>{
   return {
@@ -69,15 +70,19 @@ componentDidMount(){
     return (
       <div className='App'>
         <Header />
+        <TransitionGroup>
+        <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
         <Switch>
           <Route exact path ='/home' component={HomePage}/>
           <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes}/>}/>
           <Route path='/menu/:dishId' component={DishWithId}/>
           <Route exact path='/contactus' component={()=><Contact resetFeebackForm={this.props.resetFeebackForm}/>}/>
           <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders}/>}/>
-          <Route exact path="/"/>
+          {/* <Route exact path="/"/> */}
           <Redirect to='/home'/>
         </Switch>
+        </CSSTransition>
+        </TransitionGroup>
         <Footer/>
       </div>
     )
