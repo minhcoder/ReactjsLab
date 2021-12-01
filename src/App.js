dc45e23f628214c0bf6b240b6c67fcab77f25fce
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2021-12-01 21:30:47 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import {Navbar,NavbarBrand} from 'reactstrap'
 import React,{ Component } from 'react';
 import Main from './components/MainComponent';
-
+import {BrowserRouter} from 'react-router-dom';
 class App extends Component{
   render(){
     return(
+      <BrowserRouter>
       <div className="App">
         <Main/>
       </div>
+      </BrowserRouter>
       
     )
   }
