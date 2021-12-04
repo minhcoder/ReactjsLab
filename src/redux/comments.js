c795346e0535d0c5f31955ce0c02f6fc68da915d
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2021-12-04 22:10:31 +0700

    Combining Reducers

---
+++ src/redux/comments.js
@@ -0,0 +1,9 @@
+import { COMMENTS } from "../shared/comments";
+import { DISHES } from "../shared/dishes";
+
+export const Comments=(state=DISHES,action)=>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,19 @@
-import {createStore} from 'redux';
-import { Reducer,initialState } from './reduxer';
+import {createStore,combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Comments } from './comments';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
 
 export const ConfigureStore=()=>{
     const store=createStore(
-        Reducer,
-        initialState,
+        combineReducers({
+            dishes:Dishes,
+            leaders:Leaders,
+            comments:Comments,
+            promotions:Promotions,
+        }),
     );
     return store;
 }
